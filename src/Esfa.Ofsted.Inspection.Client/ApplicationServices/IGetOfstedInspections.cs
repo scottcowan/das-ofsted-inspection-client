99d03007eb9eeb52c205ad83fb9656defca124c7
MarkFCain <mark.cain@alimar.co.uk>
2017-10-04 14:01:56 +0100

    CI-404 corrected naming error in appveyor.yml

---
+++ src/Esfa.Ofsted.Inspection.Client/ApplicationServices/IGetOfstedInspections.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfa.Ofsted.Inspection.Client.ApplicationServices
+{
+    interface IGetOfstedInspections
+    {
+    }
+}
